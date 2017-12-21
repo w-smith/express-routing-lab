f1fd0faadec30d868f0c2327bdaaa192059f3b08
w-smith <wsmith@bex.net>
2017-12-21 08:01:30 -0700

    adding stuff and things

--- starter-code/app.js
+++ starter-code/app.js
@@ -4,14 +4,23 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-
 // How do we 'require' the candyRouter file?
-var candyRouter;
+var candyRouter = require("./candyRouter");
+var port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
+app.use('/candies', candyRouter);
+
 
 //How do we redirect the /candies path
+
+
+
+
+
 //through our candyRouter?
 //Hint: you need app.use
 
-app.listen(3000);
+app.listen(3000, function(){
+	console.log("HEY BOY HEY");
+});
--- starter-code/candyRouter.js
+++ starter-code/candyRouter.js
@@ -1,16 +1,45 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-router = express.Router();
+var router = express.Router();
 
-var candies;
+
+var candies = [{ "id":1, "name": "Snickers", "color": "Brown"},
+				{ "id":2, "name": "Skittles", "color": "All"},
+				{ "id":3, "name": "Twizzler", "color": "Red"}
+];
 
 //What would need to go into candies
 //in order to pass our first test?
 
 router.get('/', function(req,res) {
-	// What would go here? 
-	// Hint: we want all candies in JSON format
+		// What would go here? 
+		// Hint: we want all candies in JSON format
+		res.json(candies);
 });
+
+
+
+router.get('/:id', function(req, res) {
+ // SHOW
+ 	res.json(candies[req.params.id-1]);
+});
+
+
+router.post('/', function(req,res) {
+ 	candies.push(req.candies);
+	res.json(req.candies);
+});
+
+
+// router.get('/new', function(req, res) {
+//  // NEW - Typically Displays an HTML Form
+//  res.send('this would typically display an HTML form');
+// });
+
+
+// router.post('/', function(req, res) {
+//  res.send(candies);
+// });
 
 // Fill out the rest of the routes here
 
